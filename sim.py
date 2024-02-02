5dbb4ca37b893ee2ebe01cbb51a64e88231373e1
Shawn Anderson <shawn@longtailfinancial.com>
2024-02-02 12:22:32 -0800

    Add pamms and samms.

--- sim.py
+++ sim.py
@@ -4,6 +4,7 @@ import param as pm
 
 class Agent(pm.Parameterized):
     credit = pm.Number(default=0, bounds=(-100, None))
+    dollars = pm.Number(default=0, bounds=(0, None))
 
     def transfer(self, amount, to):
         if self.credit - amount < self.param["credit"].bounds[0]:
@@ -30,6 +31,28 @@ class Sim:
 
     def distribution(self):
         return sorted([agent.credit for agent in self.agents], reverse=True)
+
+
+class SAMM(Agent):
+    dollars = pm.Number(default=1, bounds=(1, None))
+    credits = pm.Number(default=1, bounds=(1, None))
+
+    def buy_credits(self, amount, agent):
+        pass
+
+    def sell_credits(self, amount, agent):
+        pass
+
+
+class PAMM(Agent):
+    dollars = pm.Number(default=1, bounds=(1, None))
+    credits = pm.Number(default=1, bounds=(1, None))
+
+    def mint_credits(self, amount, agent):
+        pass
+
+    def burn_credits(self, amount, agent):
+        pass
 
 
 s = Sim()
